e12159935bddc3771a3d1797a27a034c7c2aa9a1
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2024-10-08 21:46:56 +0530

    name changed to index,jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,44 @@
-
-import React, { useState } from 'react';
-import ProductList from './ProductList';
-import './App.css';
-import AboutUs from './AboutUs';
+import React, { useState } from 'react'
+import ProductList from './ProductList'
+import './App.css'
+import AboutUs from './index.jsx'
 
 function App() {
-  
-  const [showProductList, setShowProductList] = useState(false);
+  const [showProductList, setShowProductList] = useState(false)
 
   const handleGetStartedClick = () => {
-    setShowProductList(true);
-  };
+    setShowProductList(true)
+  }
 
   return (
     <div className="app-container">
       <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
         <div className="background-image"></div>
         <div className="content">
-         <div className="landing_content">
-         <h1>Welcome To Paradise Nursery</h1>
-          <div className="divider"></div>
-          <p>Where Green Meets Serenity</p>
-         
-          <button className="get-started-button" onClick={handleGetStartedClick}>
-            Get Started
-          </button>
-         </div>
-          <div className="aboutus_container">
-          <AboutUs/>
-          </div>
-          </div>
+          <div className="landing_content">
+            <h1>Welcome To Paradise Nursery</h1>
+            <div className="divider"></div>
+            <p>Where Green Meets Serenity</p>
 
+            <button
+              className="get-started-button"
+              onClick={handleGetStartedClick}
+            >
+              Get Started
+            </button>
+          </div>
+          <div className="aboutus_container">
+            <AboutUs />
+          </div>
+        </div>
       </div>
-      <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
+      <div
+        className={`product-list-container ${showProductList ? 'visible' : ''}`}
+      >
         <ProductList />
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
-
-
-
+export default App
